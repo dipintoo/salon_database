5fc01096bb1b1aa4d896e89eadf3f36e55cc943f
dipintoo <120252968+dipintoo@users.noreply.github.com>
2024-03-04 17:37:02 +0700

    Update salon.sh

--- salon.sh
+++ salon.sh
@@ -4,26 +4,6 @@ PSQL="psql --username=freecodecamp --dbname=salon --tuples-only -c"
 
 echo -e "\nWelcome to Dipintoo Salon! What service would you like to book?\n"
 
-main_menu() {
-  display_services_menu
-
-  read -p "Input number to book: " SERVICE_ID_SELECTED
-
-  if [[ ! $SERVICE_ID_SELECTED =~ ^[0-9]+$ ]]; then
-    main_menu "I could not find that service. What would you like today?"
-  else
-    SERVICE_NAME_SELECTED=$(get_service_name $SERVICE_ID_SELECTED)
-
-    if [[ -z $SERVICE_NAME_SELECTED ]]; then
-      main_menu "Sorry, that is not a valid service. Please choose again."
-    else
-      get_customer_details
-      book_appointment
-      display_confirmation
-    fi
-  fi
-}
-
 display_services_menu() {
   $PSQL "SELECT service_id, name FROM services ORDER BY service_id" | while read SERVICE_ID _ SERVICE_NAME; do
     echo -e "$SERVICE_ID) $SERVICE_NAME"
@@ -32,6 +12,10 @@ display_services_menu() {
 
 get_service_name() {
   $PSQL "SELECT name FROM services WHERE service_id = $1"
+}
+
+get_customer_name() {
+  $PSQL "SELECT name FROM customers WHERE phone='$1'"
 }
 
 get_customer_details() {
@@ -44,8 +28,8 @@ get_customer_details() {
   fi
 }
 
-get_customer_name() {
-  $PSQL "SELECT name FROM customers WHERE phone='$1'"
+get_customer_id() {
+  $PSQL "SELECT customer_id FROM customers WHERE phone='$1'"
 }
 
 book_appointment() {
@@ -54,13 +38,24 @@ book_appointment() {
   $PSQL "INSERT INTO appointments(service_id, customer_id, time) VALUES ($SERVICE_ID_SELECTED,$CUSTOMER_ID,'$SERVICE_TIME')"
 }
 
-get_customer_id() {
-  $PSQL "SELECT customer_id FROM customers WHERE phone='$1'"
-}
-
 display_confirmation() {
   echo -e "\nI have put you down for a $SERVICE_NAME_SELECTED at $SERVICE_TIME, $CUSTOMER_NAME."
 }
 
-# Menjalankan fungsi menu utama
-main_menu
+# Script Utama
+display_services_menu
+read -p "Input number to book: " SERVICE_ID_SELECTED
+
+if [[ ! $SERVICE_ID_SELECTED =~ ^[0-9]+$ ]]; then
+  echo "I could not find that service. What would you like today?"
+else
+  SERVICE_NAME_SELECTED=$(get_service_name $SERVICE_ID_SELECTED)
+
+  if [[ -z $SERVICE_NAME_SELECTED ]]; then
+    echo "Sorry, that is not a valid service. Please choose again."
+  else
+    get_customer_details
+    book_appointment
+    display_confirmation
+  fi
+fi
